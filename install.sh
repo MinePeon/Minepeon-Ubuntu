3d1188cd28cefdb7a7c64b74f183f7876e1bd297
mariogrip <mario.grip@gmail.com>
2014-06-23 00:51:42 +0200

    it works

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Minepeon Installation file for ubuntu
 echo "Downloading Minepeon instalation file..."
-wget 
+sudo wget --output-document=/bin/MinePeon "https://raw.githubusercontent.com/MinePeon/Minepeon-Ubuntu/master/MinePeon-Menu"
+sudo chmod +x /bin/MinePeon
